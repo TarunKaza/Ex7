155ee18cf7a4eba7c0b124345ee991e9d4ed4e75
Tarun Kaza <tarunkaza1995@gmail.como>
2016-03-25 21:22:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tarunkaza/ex7/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.tarunkaza.ex7;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    Handler handler;
+    Button button;
+    TextView heading;
+    TextView timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        button = (Button)findViewById(R.id.button);
+        heading = (TextView)findViewById(R.id.heading);
+        timer = (TextView)findViewById(R.id.timer);
+        handler = new Handler();
+        handler.postDelayed(run,1000);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                heading.setText("You've just wasted 10 seconds of your time!");
+            }
+        });
+    }
+    Runnable run = new Runnable() {
+        @Override
+        public void run() {
+            updateTime();
+        }
+    };
+
+    private void updateTime() {
+        timer.setText(String.valueOf(Integer.parseInt(timer.getText().toString())-1));
+        if(Integer.parseInt(timer.getText().toString())==0){
+            button.setVisibility(View.VISIBLE);
+            heading.setText("Hit the button");
+        }else{
+            handler.postDelayed(run,1000);
+        }
+    }
+}
